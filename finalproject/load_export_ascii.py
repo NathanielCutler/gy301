81a4993c5788482e0ac11e3c47ed14c2557cda96
Nathaniel Cutler <nathanielhcutler@gmail.com>
2024-12-16 09:12:36 -0700

    fixed nan value error

--- finalproject/load_export_ascii.py
+++ finalproject/load_export_ascii.py
@@ -15,12 +15,22 @@ import matplotlib.pyplot as plt
 # import .asc data
 ascii_grid = np.loadtxt("gy301_3dprint.asc", dtype = 'float', skiprows=6)
 
+ascii_grid [ascii_grid <0] = np.nan## gets rid of holes in the elevation data 
+ascii_grid = ascii_grid[1:-1,1:-1]
+
+
 ascii_headers = np.loadtxt("gy301_3dprint.asc", max_rows = 6, dtype = 'str')
-n_long = ascii_headers[0,1].astype(int)
-n_lat = ascii_headers[1,1].astype(int)
+n_lat, n_long = ascii_grid.shape
 dxy = ascii_headers[4,1].astype(float)
 xllcorner = ascii_headers[2,1].astype(float)
 yllcorner = ascii_headers[3,1].astype(float)
+
+
+#n_long = ascii_headers[0,1].astype(int)
+#n_lat = ascii_headers[1,1].astype(int)
+#dxy = ascii_headers[4,1].astype(float)
+#xllcorner = ascii_headers[2,1].astype(float)
+#yllcorner = ascii_headers[3,1].astype(float)
 
 ## ---- GRID FORMATION
 x = np.arange(0, dxy*n_lat, dxy) + xllcorner # array of x values
@@ -43,7 +53,7 @@ Dy = D
 ## ---- INITIAL ELEVATION
 ##for i in range(n_lat):
     ##ascii_grid[0:i] = ascii_grid [1:i]
-ascii_grid [ascii_grid <0] = np.nan## gets rid of holes in the elevation data 
+
 elv_flat = ascii_grid.flatten()
 
 
@@ -62,13 +72,14 @@ elif sy > 0.5:
     print("y is unstable")
     sys.exit()
     
-
+print (sx)
+print(sy)
 
 ### --------- PLOT INITIAL CONDITIONS -------------
 
 fig, ax = plt.subplots(1,1) # use this method
-elv_matrix = elv_flat.reshape(LAT.shape) # z is currently a 1D, but we will reshape it into the 2D coordinate form - recognizing that we want it to follow the shape of X.
-c1 = ax.pcolormesh(LONG, LAT, elv_matrix, cmap = 'viridis')
+elv_matrix_pre = elv_flat.reshape(LAT.shape) # z is currently a 1D, but we will reshape it into the 2D coordinate form - recognizing that we want it to follow the shape of X.
+c1 = ax.pcolormesh(LONG, LAT, elv_matrix_pre, cmap = 'viridis')
 fig.colorbar(c1)
 ax.set_xlabel('Distance (m)')
 ax.set_ylabel('Depth (m)')
@@ -77,7 +88,7 @@ ax.set_title('Initial conditions')
 
 fig = plt.figure()
 ax = fig.add_subplot(111,projection = '3d')
-ax.plot_surface(LONG,LAT,elv_matrix)
+ax.plot_surface(LONG,LAT,elv_matrix_pre)
 ax.set_title('inital conditions')
 ax.set_xlabel('x')
 ax.set_zlabel('z')
@@ -96,21 +107,16 @@ for i in range(n_lat):
         ### ---boundary conditions ---
         if i == 0: 
             A[ik,ik] = 1 ## no change 
-        elif i ==1:
-            A[ik,ik] = 1
        
         elif i == (n_lat-1): 
             A[ik,ik]=1
-        elif i == (n_lat-2): 
-            A[ik,ik]=1
+
         elif k == 0: 
             A[ik,ik]=1
-        elif k == 1: 
-           A[ik,ik]=1
+   
         elif k == (n_long-1):
             A[ik,ik]=1
-        elif k == (n_long-2):
-           A[ik,ik]=1
+     
         else:
             ##--- matrix coefficient time---
             A[ik,ik] = 1-2*sx -2*sy 
@@ -123,20 +129,35 @@ for i in range(n_lat):
 ### ----- RUN A QUCK CHANGE -----
 #elv_matrix += np.random.random((LAT.shape))*5 # 5 meter random additions
 
+
+
+
+
 ## Run Matrix 
 print(elv_flat)
+
+
 time = 0 
-totaltime = 1000 #years 
-while time <= totaltime: 
-    new_z = np.dot (A,elv_flat)
-    elv_flat[:] = new_z*1
+totaltime = 10000 #years 
+
+
+A_m = np.ma.masked_invalid(A)  # Mask invalid (NaN or Inf) values in matrix A
+elv_flat_m = np.ma.masked_invalid(elv_flat)  # Mask invalid values in elevation data
+
+print(np.any(np.isnan(A_m)))  # Check for NaNs in A_m
+print(np.any(np.isnan(elv_flat_m)))  # Check for NaNs in elv_flat_m
+
+while time<=totaltime:
+    newz = np.ma.dot(A_m,elv_flat_m)
+    elv_flat_m[:]= newz 
     time += dt
+
 
 print(elv_flat)
 ## plots final conditions 
 
 fig, ax = plt.subplots(1,1) # use this method
-elv_matrix = elv_flat.reshape(LAT.shape) # z is currently a 1D, but we will reshape it into the 2D coordinate form - recognizing that we want it to follow the shape of X.
+elv_matrix = elv_flat_m.reshape(LAT.shape) # z is currently a 1D, but we will reshape it into the 2D coordinate form - recognizing that we want it to follow the shape of X.
 c1 = ax.pcolormesh(LONG, LAT, elv_matrix, cmap = 'viridis')
 fig.colorbar(c1)
 ax.set_xlabel('Distance (m)')
@@ -153,7 +174,15 @@ ax.set_zlabel('z')
 ax.set_ylabel('y')
 
 
+elv_diff = elv_matrix_pre-elv_matrix
 
+fig = plt.figure()
+ax1 = fig.add_subplot(111,projection = '3d')
+ax1.plot_surface(LONG,LAT,elv_diff)
+ax1.set_title("Elevation Difference")
+ax1.set_xlabel('x')
+ax1.set_zlabel('z')
+ax1.set_ylabel('y')
 
 ### ---- SAVE ASCII OUTPUT (this can be opened in qgis easily!) -----
 header = 'NCOLS %s \n' % n_long + 'NROWS %s \n' % n_lat + 'xllcorner %s \n' % xllcorner+ 'yllcorner %s \n' % yllcorner + 'cellsize %s \n' % dxy + 'NODATA_value -9999'
